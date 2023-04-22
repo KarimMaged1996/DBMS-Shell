e4707409affdba8620601270204a6733eb04e8a0
AmrAbrazek <amr.3brazek@gmail.com>
2023-04-23 00:44:18 +0200

    add the checkname function

--- A.sh
+++ A.sh
@@ -1,29 +1,87 @@
 #!/bin/bash
 
+#get the name of the table from the user 
 function getName(){
-	echo -n "what is the name of the table you want to create? "
-	read tableName
-}
-
-
-function checkRep(){
 while true 
 do
-	getName #declare the tableName variable
-	if test -e $PWD/$tableName
-		then
-			echo "table already exist, try another name"
-			
-		else
-			
-			touch $PWD/$tableName
-			break
+	echo -n "what is the name of the table you want to create? "
+	read tableName
+	checkname $tableName
+	if [ $x -eq 1 ]; then
+		break
 	fi
 done
 }
 
-function fieldCharactristics() {
+function checkname() {
+	
+	if [[ $1 =~ ^[a-zA-Z0-9_]+\.[tT][xX][tT]$ ]]; then
+		echo "File name is valid."
+		x=1
+	else
+		echo "File name is invalid."
+		x=0
+	fi	
+}
 
+# check if a table with the same name exist in the database 
+function checkRep(){
+while true 
+do
+	getName #declare the tableName variable
+	if test -e $PWD/$tableName ;
+	then
+		echo "table already exist, try another name"
+			
+	else
+		
+		touch $PWD/$tableName
+		break
+	fi
+done
+}
+
+# assuming that only one field can be a primary key
+#'primeryKey' is turned into one when any field is declared to be a primary key
+# once any primary key is declared, no other key can be declared that is why the question will no longer be asked 
+
+function getFieldName() {
+	echo -n "what is the name of field $i? " 
+	read fieldName
+}
+
+function primeryOrNot(){
+	if [ $primeryKey -eq 0 ];
+	then
+		echo "Is field $i a primary key? "
+		select yesOrNo in "yes" "no"
+		do
+			case $yesOrNo in 
+				"yes" )
+					isPrimary="primaryKey"
+					primeryKey=1
+					break
+				;;
+				
+				"no" )
+					break
+				;;
+			
+			esac
+		done
+	fi
+}
+
+function dataType() {
+	echo "what is data type of field $i? "
+	select choice in "string" "number" 
+	do
+		choice=$choice 
+		break
+   	done 
+}
+
+function fieldCharactristics() {
 for (( i=1; i<=$1; i++ ))
 do	
 	isPrimary="notPrimary"
@@ -59,32 +117,43 @@ do
 		break
    	done 
    	
-   	
+   	#append to the file "table"
    	createField $fieldName $isPrimary $choice
 done
 }
 
+function createField() {
+	echo -n "$1,$2,$3:" >> $PWD/$tableName
+}
 
 function createTable(){
+
 
 checkRep
 
 #asking the user for the number of field and store the number in a vriable called "numOfFields"
 echo -n "how many fields in $tableName ?"
 read numOfFields
-
-# assuming that only one field can be a primary key
-#'primeryKey' is turned into one when any field is declared to be a primary key
-# once any primary key is declared, no other key can be declared that is why the question will no longer be asked  
+ 
 primeryKey=0
-fieldCharactristics $numOfFields
+
+for (( i=1; i<=$numOfFields; i++ ))
+do
+	getFieldName
+	
+	isPrimary="notPrimary"
+	primeryOrNot
+	
+	dataType
+	
+	createField $fieldName $isPrimary $choice
+done
+
 
 }
 
 
-function createField() {
-	echo -n "$1,$2,$3:" >> $PWD/$tableName
-}
+
 
 
 createTable 
