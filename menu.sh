156d9404622a5ff188625042663f88e338f5cf48
KarimMaged <karim.maged2020@yahoo.com>
2023-04-26 10:37:03 +0200

    pulling the menu.sh from the kM-branch

---
+++ menu.sh
@@ -0,0 +1,114 @@
+#! /usr/bin/bash
+
+# function to create the database
+createDatabase() {
+  # -p to avoid the error message if the root directory already exists
+  mkdir -p $PWD/root/
+  mkdir $PWD/root/"$1"
+}
+
+listDatabases() {
+  # ls the root directory where databases exist
+  # use cut to exclude the .txt in the database name
+  ls $PWD/root 
+}
+
+# create a menu
+connectToDatabase() {
+  select choice in "Create Table" "List Tables" "Drop Table" "insert into Table" " select from Table" " Delete From Table" "Update Table" "Back to main menu"; do
+    case $choice in
+    "Create Table") ;;
+    "List Tables") ;;
+    "Drop Table") ;;
+    "insert into Table") ;;
+    " select from Table") ;;
+    " Delete From Table") ;;
+    "Update Table") ;;
+    "Back to main menu")
+      menuUI
+      ;;
+    *)
+      echo $REPLY is not a valid choice
+      ;;
+    esac
+  done
+}
+
+# this function takes database name as a parameter and delete it
+dropDatabase() {
+  rm -r $PWD/root/$1
+}
+
+menuUI() {
+  select choice in "create Database" "List Databases" "Connect to Database" "Drop Database" "Exit"; do
+    case $choice in
+
+    # choice one to create a database
+    "create Database")
+      echo type the name of the database
+      read databaseName
+      while [ -z $databaseName ]; do
+        echo you Must enter a name
+        read databaseName
+      done
+
+      # to check if the database already exists
+      if [ -d $PWD/root/$databaseName ]; then
+        echo the database already exists Now you are back to the main menu
+        
+      else
+        createDatabase $databaseName
+        echo your Database was created successfully
+      fi
+      ;;
+
+    # choice to to list databases
+    "List Databases")
+      listDatabases
+      ;;
+
+    # choice three to connect to databases
+    # take the name of the database from the user and store in in a variable $databaseToConnet
+    # this variable will be used inside all the other functions
+    "Connect to Database")
+      echo which database you want to connect to
+      read databaseToConnect
+      while [ -z $databaseToConnect ]; do
+        echo you Must enter a name
+        read databaseToConnect
+      done
+      # to check if the database exist
+      if [ -d $PWD/root/$databaseToConnect ]; then
+        connectToDatabase $PWD/root/$databaseToConnect
+      else
+        echo "this database doesn't exist Now you are back to the main menu"
+      fi
+
+      ;;
+
+    #
+    "Drop Database")
+      echo type the name of the database you want to drop
+      read databaseToDrop
+      while [ -z $databaseToDrop ]; do
+        echo you Must enter a name
+        read databaseToDrop
+      done
+
+      if [ -d $databaseToDrop ]; then
+        dropDatabase $databaseToDrop
+      else
+        echo "The database you want to delete does't exist Now you are back to the main menu"
+      fi
+      ;;
+    'Exit') break
+    ;;
+    *)
+      echo $REPLY is not a valid choice
+      ;;
+    esac
+  done
+
+}
+
+menuUI
